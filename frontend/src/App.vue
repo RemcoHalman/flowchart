2720181896ad440244859890b9fc6e7d7fbd60ee
RemcoHalman <remco.halman@gmail.com>
2021-01-22 16:07:16 +0100

    feat: added basic styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="main">
+    <div class="grid-container">
+      <div class="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </div>
+      <div class="content">
+        <router-view />
+      </div>
+    </div>
   </div>
-  <router-view />
 </template>
 
 <script>
@@ -14,12 +20,48 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  padding: 0;
+}
+
+.main {
+  width: 100vw;
+}
+
+.grid-container {
+  height: 100vh;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
+  gap: 0px 0px;
+  grid-template-areas: "nav content content content content content";
+  .nav {
+    grid-area: nav;
+  }
+
+  .content {
+    grid-area: content;
+  }
+}
+
+.nav {
+  width: 100%;
+  background: #333;
+  color: #fff;
+  font-size: 1.3rem;
+  margin: 0 auto;
+  a {
+    color: white;
+  }
+}
+
+.content {
+  padding: 0 0 0 9px;
+  background: #fff;
+  background-image: radial-gradient(#ccc 10%, #fff 10%);
+  background-position: 0 0;
+  background-size: 20px 20px;
 }
 </style>
